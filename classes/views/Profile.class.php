08e153d9ba478f3e5f4373d46c43f12d7f46b5e8
Pouiko <theotimeaudaire@gmail.com>
2026-04-02 17:48:41 +0200

    add button friends

--- classes/views/Profile.class.php
+++ classes/views/Profile.class.php
@@ -542,7 +542,7 @@ if (avatarInput) {
         if (!$relation) {
             // No relation → Add Friend
             $friendBtn = '
-                <button class="prof-btn prof-btn--outline" id="friendActionBtn"
+                <button class="prof-btn prof-btn--primary" id="friendActionBtn"
                         data-action="add" data-uid="' . $profileUserId . '">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16" height="16">
                         <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"/>
@@ -552,7 +552,7 @@ if (avatarInput) {
         } elseif ($relation['status'] === 'accepted') {
             // Already friends → Remove Friend
             $friendBtn = '
-                <button class="prof-btn prof-btn--outline prof-btn--friend" id="friendActionBtn"
+                <button class="prof-btn prof-btn--primary prof-btn--friend" id="friendActionBtn"
                         data-action="remove" data-fid="' . $relation['id'] . '">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16" height="16">
                         <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM18.75 10.5a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-4.5z"/>
@@ -562,7 +562,7 @@ if (avatarInput) {
         } elseif ($relation['status'] === 'pending' && (int)$relation['sender_id'] === $currentUserId) {
             // Current user sent the request → Cancel
             $friendBtn = '
-                <button class="prof-btn prof-btn--outline" id="friendActionBtn"
+                <button class="prof-btn prof-btn--primary" id="friendActionBtn"
                         data-action="cancel" data-fid="' . $relation['id'] . '">
                     ↩️ Cancel Request
                 </button>';
